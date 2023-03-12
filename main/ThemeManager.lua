d8e6c97e8e41564976ad708271b25811b8dcf2d0
DanTHXxxs <111060565+DanTHXxxs@users.noreply.github.com>
2023-03-12 18:35:41 +0700

    Update ThemeManager.lua
    Signed-off-by: DanTHXxxs <111060565+DanTHXxxs@users.noreply.github.com>

--- main/ThemeManager.lua
+++ main/ThemeManager.lua
@@ -5,7 +5,7 @@ local ThemeManager = {} do
 
 	ThemeManager.Library = nil
 	ThemeManager.BuiltInThemes = {
-		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1c1c1c","AccentColor":"ffffff","BackgroundColor":"141414","OutlineColor":"323232"}') },
+		['Default'] 		= { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"FFFFFF","BackgroundColor":"232323","OutlineColor":"141414"}') },
 		['Sy x Hub'] 			= { 2,httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"3e70c6","BackgroundColor":"232323","OutlineColor":"141414"}') },
 	}
 
